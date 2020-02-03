d82576f3a0165757eb1b1a685b8e66dcf22b62a2
Sam Irvine <samuel.irvine711@gmail.com>
2020-02-03 09:17:43 -0400

    added README, update typography scale

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,5 +1,11 @@
 import Typography from "typography"
 import lincoln from "typography-theme-lincoln"
 
+lincoln.overrideThemeStyles = ({ scale }) => ({
+  body: {
+    ...scale(0),
+  },
+})
+
 const typography = new Typography(lincoln)
 export default typography

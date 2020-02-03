c6bf8bfe26facced7b0bcebaa6026cf0df446e4e
Sam Irvine <samuel.irvine711@gmail.com>
2020-02-03 09:44:40 -0400

    removed font-sizes, adjusted typography scale

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -3,28 +3,28 @@ import lincoln from "typography-theme-lincoln"
 
 lincoln.overrideThemeStyles = ({ scale }) => ({
   body: {
-    ...scale(0.2),
+    ...scale(0),
   },
   h1: {
-    ...scale(0.2),
+    ...scale(0.6),
   },
   h2: {
-    ...scale(0.2),
+    ...scale(0),
   },
   h3: {
-    ...scale(0.2),
+    ...scale(0),
   },
   h4: {
-    ...scale(0.2),
+    ...scale(0),
   },
   p: {
-    ...scale(0.2),
+    ...scale(0),
   },
   input: {
-    ...scale(0.2),
+    ...scale(0),
   },
   button: {
-    ...scale(0.2),
+    ...scale(0),
   },
 })
 

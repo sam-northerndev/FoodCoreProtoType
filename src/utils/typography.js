d4828b498f0a1df21fc8e914df0b5b0acb471688
Sam Irvine <samuel.irvine711@gmail.com>
2020-02-03 09:31:56 -0400

    updated sizing of typography

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -3,7 +3,28 @@ import lincoln from "typography-theme-lincoln"
 
 lincoln.overrideThemeStyles = ({ scale }) => ({
   body: {
-    ...scale(0),
+    ...scale(0.2),
+  },
+  h1: {
+    ...scale(0.2),
+  },
+  h2: {
+    ...scale(0.2),
+  },
+  h3: {
+    ...scale(0.2),
+  },
+  h4: {
+    ...scale(0.2),
+  },
+  p: {
+    ...scale(0.2),
+  },
+  input: {
+    ...scale(0.2),
+  },
+  button: {
+    ...scale(0.2),
   },
 })
 
